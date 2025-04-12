45621b20524fb98cbf1c28c3eeb3be281cf7c364
Samantha Brown <samantha.s.brwn@gmail.com>
2025-04-12 18:48:36 -0500

    Excercise 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config.from_object(Config)
 
-from app import views
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+from app import views,models
--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime,timezone
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
